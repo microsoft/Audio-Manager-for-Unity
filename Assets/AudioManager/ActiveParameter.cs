c939ee0c95dd224472058c79ac72377a9e8fe623
Joe Kelly <joekelly@microsoft.com>
2020-08-19 14:28:18 -0700

    Adding Unity project with all AudioManager code and an example script and scene

---
+++ Assets/AudioManager/ActiveParameter.cs
@@ -0,0 +1,88 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+namespace Microsoft.MixedReality.Toolkit.Audio
+{
+    /// <summary>
+    /// The runtime instance of an AudioParameter on an ActiveEvent
+    /// </summary>
+    public class ActiveParameter
+    {
+        /// <summary>
+        /// The value of the parameter
+        /// </summary>
+        private float currentValue = 0;
+        /// <summary>
+        /// The result of the parameter's curve on the currentValue
+        /// </summary>
+        private float currentResult = 0;
+        /// <summary>
+        /// Has the ActiveParameter been set to a value independent from the main AudioParameter
+        /// </summary>
+        private bool isDirty = false;
+
+        /// <summary>
+        /// Constructor: Create a new ActiveParameter from the EventParameter
+        /// </summary>
+        /// <param name="root">The EventParameter to apply to this event</param>
+        public ActiveParameter(AudioEventParameter root)
+        {
+            this.rootParameter = root;
+        }
+
+        /// <summary>
+        /// The EventParameter being used
+        /// </summary>
+        public AudioEventParameter rootParameter { get; private set; }
+        /// <summary>
+        /// The value of the root parameter, unless the ActiveParameter has been independently set
+        /// </summary>
+        public float CurrentValue
+        {
+            get
+            {
+                if (this.isDirty)
+                {
+                    return this.currentValue;
+                }
+                else
+                {
+                    return this.rootParameter.parameter.CurrentValue;
+                }
+            }
+            set
+            {
+                this.currentValue = value;
+                this.currentResult = this.rootParameter.ProcessParameter(this.currentValue);
+                this.isDirty = true;
+            }
+        }
+
+        /// <summary>
+        /// The result of the current value applied to the response curve
+        /// </summary>
+        public float CurrentResult
+        {
+            get {
+                if (this.isDirty)
+                {
+                    return this.currentResult;
+                }
+                else
+                {
+                    return this.rootParameter.CurrentResult;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Clear the modified value and use the global parameter's value
+        /// </summary>
+        public void Reset()
+        {
+            this.currentValue = this.rootParameter.parameter.CurrentValue;
+            this.currentResult = this.rootParameter.CurrentResult;
+            this.isDirty = false;
+        }
+    }
+}
---
+++ Assets/AudioManager/AudioFile.cs
@@ -0,0 +1,91 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace Microsoft.MixedReality.Toolkit.Audio
+{
+    /// <summary>
+    /// An AudioNode containing a reference to an AudioClip
+    /// </summary>
+    public class AudioFile : AudioNode
+    {
+        /// <summary>
+        /// The audio clip to be set on the AudioSource if this node is processed
+        /// </summary>
+        [SerializeField]
+        private AudioClip file = null;
+        /// <summary>
+        /// The amount of volume change to apply if this node is processed
+        /// </summary>
+        [SerializeField, Range(-1, 1)]
+        private float volumeOffset = 0;
+        /// <summary>
+        /// The amount of pitch change to apply if this node is processed
+        /// </summary>
+        [SerializeField, Range(-1, 1)]
+        private float pitchOffset = 0;
+
+        /// <summary>
+        /// Apply all modifications to the ActiveEvent before it gets played
+        /// </summary>
+        /// <param name="activeEvent">The runtime event being prepared for playback</param>
+        public override void ProcessNode(ActiveEvent activeEvent)
+        {
+            if (this.file == null)
+            {
+                Debug.LogWarningFormat("No file in node {0}", this.name);
+                return;
+            }
+
+            activeEvent.ModulateVolume(this.volumeOffset);
+            activeEvent.ModulatePitch(this.pitchOffset);
+            activeEvent.clip = this.file;
+        }
+
+#if UNITY_EDITOR
+        
+        public AudioClip File
+        {
+            get { return this.file; }
+            set { this.file = value; }
+        }
+
+        /// <summary>
+        /// The width in pixels for the node's window in the graph
+        /// </summary>
+        private const float NodeWidth = 300;
+
+        /// <summary>
+        /// EDITOR: Initialize the node's properties when it is first created
+        /// </summary>
+        /// <param name="position">The position of the new node in the graph</param>
+        public override void InitializeNode(Vector2 position)
+        {
+            this.name = "Audio File";
+            this.nodeRect.position = position;
+            this.nodeRect.width = NodeWidth;
+            AddOutput();
+            EditorUtility.SetDirty(this);
+        }
+
+        /// <summary>
+        /// EDITOR: Display the node's properties in the graph
+        /// </summary>
+        protected override void DrawProperties()
+        {
+            this.file = EditorGUILayout.ObjectField(this.file, typeof(AudioClip), false) as AudioClip;
+            if (this.file != null && this.name != this.file.name)
+            {
+                this.name = this.file.name;
+            }
+            this.volumeOffset = EditorGUILayout.Slider("Volume Offset", this.volumeOffset, -1, 1);
+            this.pitchOffset = EditorGUILayout.Slider("Pitch Offset", this.pitchOffset, -1, 1);
+        }
+
+#endif
+    }
+}
